13a981a337c4dbcd0d848e9de01d40d519487024
saar <saar.scheinkman@gmail.com>
2017-04-11 20:11:35 +0300

    Built general project structure. 2 Projects:
    TexasHoldem:
    
    Sean: TexasHoldem folder -> GameCenter
    Daniel: Loggers, Notifications
    Shachar: Game -> Card, Deck, Player
    
    Tests:
    Everyone: UnitTests
    Elad: Acceptance tests

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53e77a7c-53c0-4759-a2e8-3bfb33cdd95f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
