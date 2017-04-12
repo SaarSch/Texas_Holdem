ead5e9a6c56662675d8dc57d6f80ed9bad46f1b8
shachar <shachar3210@gmail.com>
2017-04-12 16:47:15 +0300

    update Card, Deck, Player- import from old project

--- TexasHoldem/TexasHoldem/Game/Deck.cs
+++ TexasHoldem/TexasHoldem/Game/Deck.cs
@@ -4,5 +4,55 @@ public class Deck
 {
 	public Deck()
 	{
+         public List<Card> cards = new List<Card>(52);
+
+        public Deck()
+        {
+            for (int i = 1; i < 14; i++) // init deck
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    cards.Add(new Card(i, j));
+                }
+            }
+            ShuffulDeck();
+        }
+
+        public void ShuffulDeck()
+        {
+            Random rnd = new Random();
+            for (var i = 0; i < cards.Count; i++)
+            {
+                Swap(cards, i, rnd.Next(i, cards.Count()));
+            }
+        }
+
+        public static void Swap(List<Card> list, int i, int j)
+        {
+            var temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+
+        public Card Draw()
+        {
+            if (cards.Count < 1) throw new Exception("Deck is empty");
+            Card Temp = cards[0];
+            cards.RemoveAt(0);
+            return Temp;
+        }
+
+        public Boolean Contains(int value, int type)
+        {
+           
+            for(int i = 0; i < 52; i++)
+            {
+                if (cards[i].value == value && cards[i].type == type) return true;
+            }
+            return false;
+   
+        }
+
+
 	}
 }
