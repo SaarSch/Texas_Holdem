ea8014f3bc01d154c93e2bf9039ecccc17e863e9
elad kamin <kamine@post.bgu.ac.il>
2017-04-12 18:53:55 +0300

    3 tests added added a bridges folder and a bridge interface

--- TexasHoldem/AllTests/AcceptanceTests/TestsAss1.cs
+++ TexasHoldem/AllTests/AcceptanceTests/TestsAss1.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TexasHoldem.Bridges;
 
 namespace AllTests.AcceptanceTests
 {
     [TestClass]
     public class TestsAss1
     {
+        private IBridge bridge;
         [TestMethod]
-        public void TestMethod1()
+        public void TestRegisterToTheSystemGood()
+        {
+            Assert.IsTrue(bridge.register("EladKamin","123456789"));
+            Assert.IsTrue(bridge.isUserExist("EladKamin"));
+            Assert.IsTrue(bridge.deleteUser("EladKamin"));
+        }
+        [TestMethod]
+        public void TestRegisterToTheSystemSad()
+        {
+            Assert.IsFalse(false);
+        }
+        [TestMethod]
+        public void TestRegisterToTheSystemBad()
         {
             Assert.IsFalse(false);
         }
---
+++ TexasHoldem/TexasHoldem/Bridges/IBridge.cs
@@ -0,0 +1,9 @@
+﻿namespace TexasHoldem.Bridges
+{
+    public interface IBridge
+    {
+        bool register(string v1, string v2);
+        bool isUserExist(string v);
+        bool deleteUser(string eladkamin);
+    }
+}
