fb1338daa05fceefcc61917fa21c4dd92a36569f
elad kamin <kamine@post.bgu.ac.il>
2017-04-12 18:00:24 +0300

    new branch

---
+++ TexasHoldem/AllTests/AcceptanceTests/TestsAss1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AllTests.AcceptanceTests
+{
+    [TestClass]
+    public class TestsAss1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
