1157a503d4f678c8da30fe7bbbfa4a6ab402d26d
Sean Ocheri <sean_o@walla.com>
2017-04-12 21:43:51 +0300

    registration, login & logout added and tested

---
+++ TexasHoldem/TexasHoldem/GameCenterHelpers/Pair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.GameCenterHelpers
+{
+    public class Pair<T, U>
+    {
+        public Pair()
+        {
+        }
+
+        public Pair(T First, U Second)
+        {
+            this.First = First;
+            this.Second = Second;
+        }
+
+        public T First { get; set; }
+        public U Second { get; set; }
+    }
+}
