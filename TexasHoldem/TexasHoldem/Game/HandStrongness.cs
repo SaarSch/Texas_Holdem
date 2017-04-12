df2f8a20822af82dfbf5d12f366d4efb4e0494e5
shachar <shachar3210@gmail.com>
2017-04-12 22:40:36 +0300

    update Room- add calcchips, winners, storgest hand

---
+++ TexasHoldem/TexasHoldem/Game/HandStrongness.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class HandStrongness
+{
+    public int _handValue;
+    public HandRank _handRank;
+    public List<Card> _cards;
+
+    public HandStrongness(int handValue, HandRank handRank, List<Card> cards)
+	{
+        HandValue = handValue;
+        Cards = cards;
+        HandRank = handRank;
+    }
+
+
+}
