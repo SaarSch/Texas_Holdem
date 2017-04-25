11a2df02d7822d58cec016aa7922c175497b6808
Daniel <margalda@post.bgu.ac.il>
2017-04-25 20:32:16 +0300

    added error checking for Replayer. added more tests for Replayer. added support for Replayer in Room (except for fold).

--- TexasHoldem/TexasHoldem/GameReplay/Replayer.cs
+++ TexasHoldem/TexasHoldem/GameReplay/Replayer.cs
@@ -7,28 +7,60 @@ namespace TexasHoldem.GameReplay
 {
     public class Replayer
     {
-        private static int _counter = 1;
+        private static readonly string counterPath = Directory.GetCurrentDirectory() + "\\gameReplayCounter.txt";
 
         private Replayer() { }
 
         public static string CreateReplay()
         {
-            //TODO: add error checking
+            int counter = 1;
+            if (File.Exists(counterPath))
+            {
+                counter = int.Parse(File.ReadAllText(counterPath));
+            }
 
-            var filename = "gameReplay#" + _counter + ".csv";
-            File.AppendAllText(filename, "round no, seat0, seat1, seat2, seat3, seat4, seat5, seat6, seat7, seat8, pot, community,,," + Environment.NewLine);
-            _counter++;
+            var filename = "gameReplay#" + counter + ".csv";
+            File.AppendAllText(filename, "turn no., seat0, seat1, seat2, seat3, seat4, seat5, seat6, seat7, seat8, pot, community,,," + Environment.NewLine);
+            File.WriteAllText(counterPath, "" + ++counter);
 
             return filename;
         }
 
         public static void Save(string filename, int round, List<Player> players, int pot, Card[] community, string comment)
         {
-            //TODO: add error checking
+            string path = Directory.GetCurrentDirectory() + "\\" + filename;
+            if (!File.Exists(path))
+            {
+                throw new Exception("gameReplay file does not exists");
+            }
+            if (round < 1)
+            {
+                throw new Exception("round no. is invalid");
+            }
+            if (players == null)
+            {
+                throw new Exception("player list is invalid");
+            }
+            if (players.Count == 0)
+            {
+                throw new Exception("player list is empty");
+            }
+            if (pot < 0)
+            {
+                throw new Exception("pot value is invalid");
+            }
+
             string entry = round + ",";
             foreach (Player p in players)
             {
-                entry += p.CurrentBet + ",";
+                if (!p.Folded)
+                {
+                    entry += p.CurrentBet + ",";
+                }
+                else
+                {
+                    entry += "fold,";
+                }
             }
             for (int i = 0; i <= 8-players.Count; i++)
             {
@@ -39,7 +71,10 @@ namespace TexasHoldem.GameReplay
             {
                 for (int i = 0; i < community.Length; i++)
                 {
-                    entry += community[i].value + community[i].type.ToString().Substring(0,1) + ";";
+                    if (community[i] != null)
+                    {
+                        entry += community[i].value + community[i].type.ToString().Substring(0, 1) + ";";
+                    }
                 }
             }
             else
@@ -47,7 +82,8 @@ namespace TexasHoldem.GameReplay
                 entry += "-";
             }
             entry += ",,," + comment;
-            File.AppendAllText(Directory.GetCurrentDirectory() + "\\" + filename, entry + Environment.NewLine);
+
+            File.AppendAllText(path, entry + Environment.NewLine);
         }
     }
 }
