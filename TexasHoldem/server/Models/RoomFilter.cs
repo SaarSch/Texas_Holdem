041ca460ff366663d4507089c551c4509e64c66a
shachar <shachar3210@gmail.com>
2017-05-10 18:00:48 +0300

    add room, roomfilter models

---
+++ TexasHoldem/server/Models/RoomFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace server.Models
+{
+    public class RoomFilter
+    {
+        public string player_name;
+        public int pot_size;
+        public bool league_only;
+        public string game_type;
+        public int buy_in_policy;
+        public int chip_policy;
+        public int min_players;
+        public int max_players;
+        public bool sepctating_allowed;
+    }
+}
