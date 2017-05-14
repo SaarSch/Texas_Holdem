d277cc2881e3b3b3a868c0fb82d3c832281940a4
elad kamin <kamine@post.bgu.ac.il>
2017-05-14 15:07:15 +0300

    all funcanalty trensfer to usercontroller

--- TexasHoldem/server/Controllers/UserController.cs
+++ TexasHoldem/server/Controllers/UserController.cs
@@ -4,16 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using server.Models;
 
 namespace server.Controllers
 {
     public class UserController : ApiController
     {
-        // GET: api/User
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
         // GetRank -->GET: api/User/elad
         public int Get(string username)
         {
@@ -53,7 +49,7 @@ namespace server.Controllers
             return "";
         }
         // DeleteUser -->GET: api/User?username=elad&passwordOrRank=123456&mod=delete
-        //mode: delete | changerank
+        //mode: delete | changerank | register
         public string Get(string username,string passwordOrRank ,string mode)
         {
             try
@@ -66,6 +62,9 @@ namespace server.Controllers
                     case "changerank":
                         WebApiConfig.UserManger.ChangeRank(username,Int32.Parse(passwordOrRank) );
                         break;
+                    case "register":
+                        WebApiConfig.UserManger.Register(username, passwordOrRank);
+                        break;
                     default:
                         throw new Exception("comunication error: unkown mode");
                 }
@@ -76,15 +75,43 @@ namespace server.Controllers
             }
             return "";
         }
-        // POST: api/User
-        public void Post([FromBody]string value)
+        // login -->POST: api/Login
+        public UserData Post([FromBody]UserData value)
         {
+            var ret = new UserData();
+            try
+            {
+                var u = WebApiConfig.UserManger.Login(value.Username, value.Password);
+                ret.AvatarPath = u.GetAvatar();
+                ret.Chips = u.ChipsAmount;
+                ret.Email = u.GetEmail();
+                ret.Password = u.GetPassword();
+                ret.Rank = u.League;
+                ret.Username = u.GetUsername();
+                ret.Wins = u.Wins;
+            }
+            catch (Exception e)
+            {
+                ret.Message = e.Message;
+            }
+
+            return ret;
+        }
+        //editUser --> POST: api/Login?username=elad
+        public string Post([FromBody]UserData value, string username)
+        {
+            try
+            {
+                WebApiConfig.UserManger.EditUser(username, value.Username, value.Password, value.AvatarPath, value.Email);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+            return "";
         }
 
-        // PUT: api/User/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
 
         // DELETE: api/User/5
         public void Delete(int id)
