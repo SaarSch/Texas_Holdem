6d810a8f9e39288727b71ffb0efd47c1752c833e
shachar <shachar3210@gmail.com>
2017-05-17 13:19:06 +0300

    add Replay controller

---
+++ TexasHoldem/server/Controllers/ReplayController.cs
@@ -0,0 +1,24 @@
+﻿using server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace server.Controllers
+{
+    public class ReplayController : ApiController
+    {
+        // GET: api/Replay
+        public IEnumerable<RoomState> Get(string RoomName, string Player)
+        {
+            List<RoomState> ans = null;
+            if(RoomController.Replays.ContainsKey(RoomName) && RoomController.Replays[RoomName].ContainsKey(Player))
+            ans = RoomController.Replays[RoomName][Player];
+            return ans;
+        }
+
+      
+    }
+}
