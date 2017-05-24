479d4ece2bbbe997b4bd4dbf6e76753034ac53d9
shachar <shachar3210@gmail.com>
2017-05-24 14:29:23 +0300

    add lock to logger

--- TexasHoldem/TexasHoldem/Loggers/Logger.cs
+++ TexasHoldem/TexasHoldem/Loggers/Logger.cs
@@ -11,66 +11,57 @@ namespace TexasHoldem.Loggers
         Error, //logs to Error Log and prints to the screen
         Exception //only logs to Error Log
     }
-    
-  public class Logger
-  {
-      public static string AppDataPath, ErrorPath, ActionPath;
-	  private const int NumberOfRetries = 3;
-	  private const int DelayOnRetry = 1000;
 
-		private Logger() { }
+    public class Logger
+    {
+        private static Object thisLock = new Object();
+        public static string AppDataPath, ErrorPath, ActionPath;
+        private const int NumberOfRetries = 3;
+        private const int DelayOnRetry = 1000;
 
-      public static void Log(Severity s, string msg)
-      {
-          // if 'log' was called from the server project
-          AppDataPath = AppDomain.CurrentDomain.GetData("DataDirectory") != null ? AppDomain.CurrentDomain.GetData("DataDirectory").ToString() : AppDomain.CurrentDomain.BaseDirectory;
-          ErrorPath = AppDataPath + "\\errorLog.txt";
-          ActionPath = AppDataPath + "\\actionLog.txt";
+        private Logger() { }
 
-          if (msg == "")
-          {
-              var exception = new Exception("message is empty.");
-              Log(Severity.Exception, exception.Message);
-              throw exception;
+        public static void Log(Severity s, string msg)
+        {
+            lock (thisLock)
+            {
+                // if 'log' was called from the server project
+                AppDataPath = AppDomain.CurrentDomain.GetData("DataDirectory") != null ? AppDomain.CurrentDomain.GetData("DataDirectory").ToString() : AppDomain.CurrentDomain.BaseDirectory;
+                ErrorPath = AppDataPath + "\\errorLog.txt";
+                ActionPath = AppDataPath + "\\actionLog.txt";
+
+                if (msg == "")
+                {
+                    var exception = new Exception("message is empty.");
+                    Log(Severity.Exception, exception.Message);
+                    throw exception;
+                }
+
+                var action = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ": " + msg;
+                var error = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ": " +
+                           s + ": " + msg;
+                    switch (s)
+                    {
+                        case Severity.Exception:
+                            File.AppendAllText(ErrorPath, error + Environment.NewLine);
+                            break;
+                        case Severity.Error:
+                            File.AppendAllText(ErrorPath, error + Environment.NewLine);
+                            Console.WriteLine(error);
+                            break;
+                        case Severity.Action:
+                            File.AppendAllText(ActionPath, action + Environment.NewLine);
+                            Console.WriteLine(action);
+                            break;
+                        case Severity.Warning:
+                            Console.WriteLine(error);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(s), s, null);
+                    }
+                    // Do stuff with file
+                    // When done we can break loop
             }
-
-          var action = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ": " + msg;
-          var error = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ": " +
-                     s + ": " + msg;
-
-	      for (int i=1; i <= NumberOfRetries; ++i) {
-		      try {
-			      switch (s)
-			      {
-				      case Severity.Exception:
-					      File.AppendAllText(ErrorPath, error + Environment.NewLine);
-					      break;
-				      case Severity.Error:
-					      File.AppendAllText(ErrorPath, error + Environment.NewLine);
-					      Console.WriteLine(error);
-					      break;
-				      case Severity.Action:
-					      File.AppendAllText(ActionPath, action + Environment.NewLine);
-					      Console.WriteLine(action);
-					      break;
-				      case Severity.Warning:
-					      Console.WriteLine(error);
-					      break;
-				      default:
-					      throw new ArgumentOutOfRangeException(nameof(s), s, null);
-			      }
-					// Do stuff with file
-					break; // When done we can break loop
-		      }
-		      catch (IOException e) {
-			      // You may check error code to filter some exceptions, not every error
-			      // can be recovered.
-			      if (i == NumberOfRetries) // Last one, (re)throw exception and exit
-			      throw;
-
-			      Thread.Sleep(DelayOnRetry);
-		      }
-			}
-      }
-  }
+        }
+    }
 }
