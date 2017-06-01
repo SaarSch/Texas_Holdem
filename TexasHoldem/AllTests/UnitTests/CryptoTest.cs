fae051c73203de5a5769790f9b64cd951874ee9e
shachar <shachar3210@gmail.com>
2017-06-01 11:02:07 +0300

    rename CryptoTest

--- TexasHoldem/AllTests/UnitTests/CryptoTest.cs
+++ TexasHoldem/AllTests/UnitTests/CryptoTest.cs
@@ -5,7 +5,7 @@ using server;
 namespace AllTests.UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class CryptoTest
     {
         [TestMethod]
         public void TestMethod1()
