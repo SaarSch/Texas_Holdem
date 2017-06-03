3b10c29ef03640fda9b19bbdf801e21bbd07b2c8
shachar <shachar3210@gmail.com>
2017-06-03 12:07:10 +0300

    add test to crypto class

--- TexasHoldem/AllTests/UnitTests/CryptoTest.cs
+++ TexasHoldem/AllTests/UnitTests/CryptoTest.cs
@@ -32,7 +32,7 @@ namespace AllTests.UnitTests
         public void TestMethod3()
         {
             int test = 1001;
-            string encrypt = Crypto.Encrypt(test);
+            string encrypt = Crypto.Encrypt(""+test);
             string dec = Crypto.Decrypt(encrypt);
             Assert.AreNotEqual(test, encrypt);
             Assert.AreEqual(test, Convert.ToInt32(dec));
