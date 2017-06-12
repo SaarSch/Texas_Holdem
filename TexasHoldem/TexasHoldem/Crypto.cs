55af6c00af54b6853a3e98c04145c83d7206af16
elad kamin <kamine@post.bgu.ac.il>
2017-06-12 22:23:48 +0300

    encrypt decrypt are here

---
+++ TexasHoldem/TexasHoldem/Crypto.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace TexasHoldem
+{
+    public class Crypto
+    {
+        static readonly string PasswordHash = "P@@Sw0rd";
+        static readonly string SaltKey = "S@EL&ADY";
+        static readonly string VIKey = "@1B2c3D4e5F6g7H8";
+
+        public static string Encrypt(string plainText)
+        {
+            int mod4 = plainText.Length % 4;
+            if (mod4 > 0)
+            {
+                plainText += new string('*', 4 - mod4);
+            }
+
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+            var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+
+            byte[] cipherTextBytes;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+                    cipherTextBytes = memoryStream.ToArray();
+                    cryptoStream.Close();
+                }
+                memoryStream.Close();
+            }
+
+            StringBuilder crypt = new StringBuilder(Convert.ToBase64String(cipherTextBytes));
+            if (crypt.Length > 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (crypt[crypt.Length - 1 - i] == '=')
+                    {
+                        crypt[crypt.Length - 1 - i] = '*';
+                    }
+                    else break;
+                }
+            }
+            return crypt.ToString();
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            encryptedText=encryptedText.Replace(' ', '+');
+            StringBuilder crypt = new StringBuilder(encryptedText);
+            if (crypt.Length > 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (crypt[crypt.Length - 1 - i] == '*')
+                    {
+                        crypt[crypt.Length - 1 - i] = '=';
+                    }
+                    else break;
+                }
+            }
+            byte[] cipherTextBytes = Convert.FromBase64String(crypt.ToString());
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            string ans = Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+            int mod = ans.Length % 4;
+            if (ans != null && ans.Length > 3)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (ans[ans.Length - 1] == '*')
+                    {
+                        ans = ans.Substring(0, ans.Length - 1 - i);
+                    }
+                }
+            }
+
+            return ans.ToString();
+        }
+
+    }
+}
