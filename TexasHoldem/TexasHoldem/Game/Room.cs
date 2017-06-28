3833a22cb416068de8ee936b6bd214b5df4bd1b5
shachar <shachar3210@gmail.com>
2017-06-28 13:08:07 +0300

    fix pot limit logic

--- TexasHoldem/TexasHoldem/Game/Room.cs
+++ TexasHoldem/TexasHoldem/Game/Room.cs
@@ -470,7 +470,7 @@ namespace TexasHoldem.Game
                     }
             }
 
-            if (!smallBlind && GamePreferences.GameType == Gametype.Limit) // limit pot
+            if (!smallBlind && GamePreferences.GameType == Gametype.PotLimit) // limit pot
             {
                 var pot = 0;
                 foreach (var p1 in Players) pot += p1.CurrentBet;
